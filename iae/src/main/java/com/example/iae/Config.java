345e6d37141a52c4eb25500c49fcaabaf5f57919
Arda Kestane <85238789+ArdaKestane@users.noreply.github.com>
2023-05-09 01:12:14 +0300

    Config read and write functions

---
+++ iae/src/main/java/com/example/iae/Config.java
@@ -0,0 +1,49 @@
+package com.example.iae;
+
+import java.io.Serializable;
+
+public class Config implements Serializable{
+    private String name;
+    private String source;
+    private String libraries; // ArrayList?
+    private String compilerPath;
+
+    public Config(String name, String source, String libraries, String compilerPath) {
+        this.name = name;
+        this.source = source;
+        this.libraries = libraries;
+        this.compilerPath = compilerPath;
+    }
+
+    public String getname() {
+        return name;
+    }
+    public void setname(String name) {
+        this.name = name;
+    }
+
+    public String getSource() {
+        return source;
+    }
+    public void setSource(String source) {
+        this.source = source;
+    }
+    public String getLibraries() {
+        return libraries;
+    }
+    public void setLibraries(String libraries) {
+        this.libraries = libraries;
+    }
+    public String getCompilerPath() {
+        return compilerPath;
+    }
+    public void setCompilerPath(String compilerPath) {
+        this.compilerPath = compilerPath;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
