76922783af8ba60a267092a3a6dc0585af37c1c9
UmutKarakuyu <umutk@windowslive.com>
2023-05-10 00:13:47 +0300

    base code for projectListView in main screen

--- iae/src/main/java/com/example/iae/MainController.java
+++ iae/src/main/java/com/example/iae/MainController.java
@@ -5,13 +5,21 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.util.ArrayList;
+import java.util.Objects;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -19,7 +27,7 @@ public class MainController {
     @FXML
     private VBox projectScene;
     @FXML
-    private ListView<Project> projectList;
+    private ListView<HBox> projectList;
     @FXML
     private Label projectName;
     @FXML
@@ -37,8 +45,43 @@ public class MainController {
     public void initialize() {
         configurationsList = readConfigurationsFromFile();
 
+        /* test data
+        Project p1 = new Project("1", "","",new Config("","","",""));
+        Project p2 = new Project("2", "","",new Config("","","",""));
+        Project p3 = new Project("3", "","",new Config("","","",""));
+        Project p4 = new Project("4", "","",new Config("","","",""));
+
+        projects.add(p1);   projects.add(p2);
+        projects.add(p3);   projects.add(p4);
+        */
         //projects = projects from database
-        projectList.getItems().addAll(projects);
+        for (Project p: projects) {
+            HBox hbox = new HBox();
+            HBox trash = new HBox();
+            trash.setAlignment(Pos.CENTER_RIGHT);
+            HBox.setHgrow(trash,Priority.ALWAYS);
+            HBox.setHgrow(hbox,Priority.ALWAYS);
+            hbox.setSpacing(10);
+            hbox.setAlignment(Pos.CENTER_LEFT);
+            HBox.setMargin(hbox, new Insets(0, 0, 10, 0));
+            Button deleteButton = new Button();
+            ImageView deleteImageView = new ImageView(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/trash.png"))));
+            deleteButton.setGraphic(deleteImageView);
+            deleteImageView.setFitWidth(20);
+            deleteImageView.setFitHeight(20);
+            deleteButton.setStyle("-fx-background-color: transparent; -fx-padding: 0;");
+            Label label = new Label(p.getName());
+            trash.getChildren().add(deleteButton);
+            hbox.getChildren().addAll(label, trash);
+            hbox.setOnMouseClicked(event -> {
+                displayProject(p);
+            });
+            deleteButton.setOnAction(event -> {
+                projectList.getItems().remove(hbox);
+                deleteProject(p);
+            });
+            projectList.getItems().add(hbox);
+        }
     }
 
     @FXML
@@ -79,10 +122,8 @@ public class MainController {
         stage.showAndWait();
     }
 
-    @FXML
-    private void displayProject() {
+    private void displayProject(Project selectedProject) {
         projectScene.setVisible(!projectScene.isVisible()); // I will delete this later
-        selectedProject = projectList.getSelectionModel().getSelectedItem();
         if(selectedProject != null){
             projectName.setText(selectedProject.getName());
             compilerName.setText(selectedProject.getConfiguration().getSource());
@@ -93,8 +134,7 @@ public class MainController {
         //else
             //projectScene.setVisible(false);
     }
-    @FXML
-    private void deleteProject(){
+    private void deleteProject(Project selectedProject){
         projects.remove(selectedProject);
         // remove it from the database as well
         projectScene.setVisible(false);
