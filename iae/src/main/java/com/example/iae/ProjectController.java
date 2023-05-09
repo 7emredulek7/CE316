f4e643127432b91c766752d1d50caacd4dc92f9f
Fatma Melike Buyruk <80330367+mellkie@users.noreply.github.com>
2023-05-09 17:26:26 +0300

    implemented methods for compiling languages from command prompt

--- iae/src/main/java/com/example/iae/ProjectController.java
+++ iae/src/main/java/com/example/iae/ProjectController.java
@@ -1,7 +1,9 @@
 package com.example.iae;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -15,7 +17,7 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
-public class ProjectController extends MainController{
+public class ProjectController extends MainController {
     @FXML
     private TextField name;
     @FXML
@@ -30,6 +32,7 @@ public class ProjectController extends MainController{
     public void initialize() {
         // configurations.getItems.addAll()
     }
+
     @FXML
     private void editConfiguration() throws IOException {
         configurationStatus = "edit selected configuration";
@@ -104,5 +107,47 @@ public class ProjectController extends MainController{
 
         Stage stage = (Stage) name.getScene().getWindow();
         stage.fireEvent(new WindowEvent(stage, WindowEvent.WINDOW_CLOSE_REQUEST));
+    }
+
+    private void compileProject(String language, String projectName, String path) {
+        Stage stage = (Stage) name.getScene().getWindow();
+        stage.fireEvent(new WindowEvent(stage, WindowEvent.WINDOW_CLOSE_REQUEST));
+        String command = "default";
+        String[] commandArgs = command.split("\\s+");
+        switch (language) {
+            case "C":
+                command = "cmd.exe /c gcc -o " + projectName + ".exe " + projectName + ".c & " + projectName;
+                break;
+            case "C++":
+                command = "cmd.exe /c g++ -o " + projectName + ".exe " + projectName + ".cpp & " + projectName;
+                break;
+            case "Python":
+                command = "cmd.exe /c py " + projectName + ".py";
+                break;
+            case "Java":
+                command = "cmd.exe /c javac " + projectName + ".java & java " + projectName;
+                break;
+            case "Dart":
+                command = "cmd.exe /c dart run " + projectName + ".dart";
+                break;
+            default:
+                break;
+        }
+        try {
+            commandArgs = command.split("\\s+");
+            ProcessBuilder builder = new ProcessBuilder(commandArgs);
+            builder.directory(new File(path));
+            builder.redirectErrorStream(true);
+            Process process = builder.start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            int exitCode = process.waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 }
